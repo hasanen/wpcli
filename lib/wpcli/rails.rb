43fadb9c9dcb264cd8d2116498573b25d5b108b5
Joni Hasanen <joni.hasanen@pieceofcode.net>
2014-02-25 20:58:12 +0200

    Added check for nil

--- lib/wpcli/rails.rb
+++ lib/wpcli/rails.rb
@@ -1,24 +1,26 @@
 module Wpcli
-  def wpcli key
-    load_apps if @apps.nil?
-    @apps[key]
-  end
+  module RailsExt
+    def wpcli key
+      load_apps if @apps.nil?
+      @apps[key]
+    end
 
-  def wp_apps?
-    load_apps if @apps.nil?
-    !@apps.empty?
-  end
+    def wp_apps?
+      load_apps if @apps.nil?
+      !@apps.empty?
+    end
 
-  private
+    private
 
-  def load_apps
-    file = File.read("#{Rails.root}/config/wpcli.yml")
-    config = YAML.load(file)
+    def load_apps
+      file = File.read("#{Rails.root}/config/wpcli.yml")
+      config = YAML.load(file)
 
-    @apps = {}
-    if config.has_key?("apps") && !config["apps"].nil?
-      config["apps"].each do |key, path|
-        @apps[key.to_sym] = Client.new path
+      @apps = {}
+      if config.has_key?("apps") && !config["apps"].nil?
+        config["apps"].each do |key, path|
+          @apps[key.to_sym] = Client.new path
+        end
       end
     end
   end
